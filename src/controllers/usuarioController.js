d4fbca7aa17c61d076274a133733494232f290dd
mateus.galeani <mateus.galeani@sptech.school>
2026-09-09 20:12:38 -0300

    Corrigindo erros

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -44,7 +44,7 @@ function autenticar(req, res) {
                     console.log(erro);
                     console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
                     res.status(500).json(erro.sqlMessage);
-                }
+                }   
             );
     }
 
